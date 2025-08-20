dde3f7b2a219bb734c5adc0c21b3994193ce600c
Raphael <95745431+lemrex@users.noreply.github.com>
2025-08-20 13:22:39 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Node.js in Docker!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
